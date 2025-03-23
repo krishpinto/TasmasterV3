e312e15f60e6c57bb5a7b49e9170a3c6307e9844
krishpinto <developerkporiginal@gmail.com>
2025-03-23 20:44:29 +0530

    end commit

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQzArcUQBVKb6HvMXbEt9BREIFa7SLaO8",
-  authDomain: "taskmaster-9f604.firebaseapp.com",
-  projectId: "taskmaster-9f604",
-  storageBucket: "taskmaster-9f604.firebasestorage.app",
-  messagingSenderId: "345286836400",
-  appId: "1:345286836400:web:afd85037b0937966b73b75",
-  measurementId: "G-VN7EH4MVVM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
